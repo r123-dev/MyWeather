e5aafcca8c7e6936786f273ecae144f8f4f18ae1
Vivek Mourya <rohanmourya879@gmail.com>
2024-06-08 00:07:29 +0530

    completing weatherapp task

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+<img src="https://www.htmlhints.com/image/react/reactWeatherApp.png" width="100%">
+</p>
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  key: "c843434697e3f70584a53d837483069b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
